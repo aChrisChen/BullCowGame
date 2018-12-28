18b592e7c81eac8974c8d7b29001d7ee11efbe27
aChrisChen <chenjipengpeng@163.com>
2018-12-28 14:57:03 -0500

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by 陈吉鹏 on 12/28/18.
+//  Copyright © 2018 Chris. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
